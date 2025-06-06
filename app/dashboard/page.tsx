485d2bf5fe647680b2b4cc6ca1fa1fae5898e284
SotaMiyashita <sota.miyashita@dcnc.co.jp>
2025-06-06 16:43:01 +0900

    fix: Dashboardページ修正

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,7 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
-
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+export default function DashboardPage() {
+    return (
+      <div>
+        <h1>ようこそ ダッシュボードページ！</h1>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-        {children}
-      </div>
-    </div>
-  );
-}
+    );
+  }
